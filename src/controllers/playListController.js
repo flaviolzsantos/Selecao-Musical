6cd73b79ca42f7e18701d7820cfe048d2dda9e51
flaviolzsantos <flaviolzsantos@gmail.com>
2024-09-21 18:07:39 -0300

    add controller getPlayList

---
+++ src/controllers/playListController.js
@@ -0,0 +1,28 @@
+
+import getPlaylistByTemperature from '../services/playlistService';
+import getWeatherData from '../config/weatherApi';
+
+export default async function getPlaylist(req, res) {
+  const city = req.query.city;
+
+  if (!city) {
+    return res.status(400).json({ error: 'City parameter is required' });
+  }
+
+  try {
+    const weatherData = await getWeatherData(city);
+    const temperature = weatherData.temp;
+    const playlist = getPlaylistByTemperature(temperature);
+
+    res.json({
+      city: city,
+      temperature: temperature,
+      playlist: playlist
+    });
+    
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getPlaylist };
